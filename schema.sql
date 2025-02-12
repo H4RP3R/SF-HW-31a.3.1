05d57a62513c42f076e05015c80e87359afa094f
zombiehunter <viktormallory25@gmail.com>
2025-02-12 20:07:17 +0300

    fix(schema): update database schema and remove sample data

--- schema.sql
+++ schema.sql
@@ -1,17 +1,20 @@
+DROP DATABASE IF EXISTS gonews;
+CREATE DATABASE gonews;
+
+\c gonews;
+
 DROP TABLE IF EXISTS posts, authors;
 
 CREATE TABLE authors (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE posts (
-    id SERIAL PRIMARY KEY,
-    author_id INTEGER REFERENCES authors(id) NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    author_id BIGINT REFERENCES authors(id) NOT NULL,
     title TEXT  NOT NULL,
     content TEXT NOT NULL,
-    created_at BIGINT NOT NULL
+    created_at BIGINT NOT NULL,
+    published_at BIGINT
 );
-
-INSERT INTO authors (id, name) VALUES (0, 'Дмитрий');
-INSERT INTO posts (id, author_id, title, content, created_at) VALUES (0, 0, 'Статья', 'Содержание статьи', 0);
