7e0beb5cdeb242f1bb7e587d6c61cf02369fdc8c
zombiehunter <viktormallory25@gmail.com>
2025-02-16 13:58:00 +0300

    feat(postgres): add Config.String() method with password redaction

--- pkg/storage/postgres/config.go
+++ pkg/storage/postgres/config.go
@@ -1,6 +1,9 @@
 package postgres
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Config struct {
 	User     string
@@ -12,4 +15,14 @@ type Config struct {
 
 func (c *Config) ConString() string {
 	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s", c.User, c.Password, c.Host, c.Port, c.DBName)
+}
+
+func (c Config) String() string {
+	var sb strings.Builder
+	for i := 0; i < len([]rune(c.Password)); i++ {
+		sb.WriteString("*")
+	}
+	c.Password = sb.String()
+
+	return fmt.Sprintf("%#v", c)
 }
